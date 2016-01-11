cc8f3c4acc01f6f6938c5e0abd62bffd6d439d25
Bryan Trinh <bryantrinh91@outlook.com>
2016-01-10 20:25:46 -0600

    Create file_open.pl

---
+++ file_open.pl
@@ -0,0 +1,25 @@
+#Use this script for opening files
+#
+#User inputs: input file name, output file name in terminal/console
+
+use strict;
+use warnings;
+
+print "File name: ";
+my $name = <STDIN>;
+chomp($name);
+
+print "Output file name: ";
+my $filename = <STDIN>;
+chomp($filename);
+
+open(my $fh, '<', $name) 
+	or die "Could not open file '$name' $!";
+
+open(my $outfile, '>', $filename) 
+	or die "Could not open file '$filename' $!";
+
+while (my $row = <$fh>) {
+	chomp $row;
+	#your code here
+}
